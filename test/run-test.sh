2395b0dd332e6f7867646b55f5f3d2124128fb72
Anders Eknert <anders.eknert@bisnode.com>
2020-07-18 23:16:40 +0200

    Add initial test script and resources

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+kind create cluster --name=kind
+
+kubectl --context=kind-kind create secret tls tls-terminator-cert --cert=cert/tls.crt --key=cert/tls.key
+kubectl --context=kind-kind create service clusterip nginx-tls-terminator --tcp=443:8443
+kubectl --context=kind-kind apply -f deployment.yaml
+
 # docker run --user 1000 -p 8443:8443 -v ${PWD}/test/cert:/etc/nginx/ssl nginx-tls-terminator:latest
 
 # Create self signed cert
