684802a0a917c31880d092afe73b3cb1ea85361f
Anders Eknert <anders.eknert@bisnode.com>
2020-07-19 16:26:22 +0200

    Add complete end-to-end test

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
 
-kind create cluster --name=kind
+# Pre-requisites:
+# Self-signed cert created with..
+#
+# openssl req -nodes -x509 -newkey rsa:4096 -keyout cert/tls.key -out cert/tls.crt -days 10000 -subj "/C=SE/ST=Stockholm/L=Stockholm/O=eknert/OU=Org/CN=nginx-tls-terminator.default.svc.cluster.local"
+#
+# ..and placed in certs/
 
-kubectl --context=kind-kind create secret tls tls-terminator-cert --cert=cert/tls.crt --key=cert/tls.key
-kubectl --context=kind-kind create service clusterip nginx-tls-terminator --tcp=443:8443
-kubectl --context=kind-kind apply -f deployment.yaml
+set -eu -o pipefail
 
-# docker run --user 1000 -p 8443:8443 -v ${PWD}/test/cert:/etc/nginx/ssl nginx-tls-terminator:latest
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-# Create self signed cert
-# openssl req -nodes -x509 -newkey rsa:4096 -keyout cert/tls.key -out cert/tls.crt -days 10000 -subj "/C=SE/ST=Stockholm/L=Stockholm/O=eknert/OU=Org/CN=localhost
+test_cluster_name=nginx-tls-terminator-test
 
-# Store it to tls-terminator-cert secret
+kind create cluster --name="${test_cluster_name}"
 
-# Deploy the service and deployment
+k() {
+    kubectl --context kind-${test_cluster_name} "$@"
+}
 
-# Query the service to make sure it works as expected
+k create secret tls tls-terminator-cert --cert="${DIR}"/cert/tls.crt --key="${DIR}"/cert/tls.key
+k create service clusterip nginx-tls-terminator --tcp=443:8443
+k apply -f "${DIR}"/deployment.yaml
+
+# Wait some time for the pods to be created..
+sleep 20
+
+# ..and even more time for them to be ready
+k wait pods -l app=nginx-tls-terminator --for=condition=Ready --timeout=90s
+
+nginx_pod=$(k get pod -l app=nginx-tls-terminator -o name)
+
+k cp "${DIR}"/cert/tls.crt "${nginx_pod:4}:/tmp" -c nginx
+k exec "${nginx_pod}" -c nginx -- curl \
+    --fail -s -o /dev/null -I -w "%{http_code}" \
+    --cacert /tmp/tls.crt \
+    https://nginx-tls-terminator.default.svc.cluster.local
+
+# Cleanup
+kind delete cluster --name "${test_cluster_name}"
